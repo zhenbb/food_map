b6d3681fa5ba09d17dd0c3cb13b8a27ef32ea920
ZHEN <belong1201@gmail.com>
2023-04-25 16:06:12 +0800

    no message

---
+++ src/test/java/com/example/food_map/FoodMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
